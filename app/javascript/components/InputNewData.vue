9c47b5986cc3d318c151d98ad73ee523786a0b37
a-kit-2 <akito.nakamura.c@gmail.com>
2021-05-30 01:11:19 +0900

    データを入力するコンポーネントを作りました。

---
+++ app/javascript/components/InputNewData.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>新しいデータを入力する場所</div>
+</template>
+
+<script>
+export default {
+  props: { start_time: String },
+  setup(props) {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
--- app/javascript/home.vue
+++ app/javascript/home.vue
@@ -1,16 +1,19 @@
 <template>
   <Header v-bind:username="jsonData.username"></Header>
   <RecordWorking v-bind:start_time="jsonData.start_time"></RecordWorking>
+  <InputNewData></InputNewData>
 </template>
 
 <script>
 import Header from "./components/Header";
 import RecordWorking from "./components/RecordWorking";
+import InputNewData from "./components/InputNewData";
 
 export default {
   components: {
     Header,
     RecordWorking,
+    InputNewData,
   },
   props: {
     jsonData: {
